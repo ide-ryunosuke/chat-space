63cc60220e79767c4a59f88f5ba82eaf24685fd5
ide-ryunosuke <r.i1996423@gmail.com>
2019-02-25 19:27:43 +0900

    追加機能、削除機能実装

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,22 +1,22 @@
 $(function() {
 
   var search_list = $("#user-search-result");
-  var memver_list = $("#chat-group-users");
+  var member_list = $("#chat-group-users");
 
   function appendUserHtml(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">
                     ${user.name}
                   </p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
                 </div>`
     search_list.append(html);
   }
 
-  function appendMembers(user_name,user_id) {
+  function appendMemberHTML(name,user_id) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
-                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
+                  <p class='chat-group-user__name'>${name}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
     member_list.append(html);
@@ -35,7 +35,6 @@ $(function() {
       if (users.length !== 0) {
         users.forEach(function(user) {
           appendUserHtml(user);
-          console.log(this)
         });
       }
     })
@@ -43,4 +42,15 @@ $(function() {
       alert('ユーザー検索に失敗しました');
     });
   });
+  $("#user-search-result").on("click",".user-search-add",function(){
+    var user_id = $(this).attr("data-user-id");
+    var name = $(this).attr("data-user-name");
+    $(this).parent().remove();
+    appendMemberHTML(name,user_id)
+  });
+  $("#chat-group-users").on("click",".chat-group-user",function(){
+    $(this).remove();
+  });
 });
+
+
