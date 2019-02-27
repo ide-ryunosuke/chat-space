61b861de17265010d6389d8618c937d0a3df9e31
ide-ryunosuke <r.i1996423@gmail.com>
2019-02-27 14:33:38 +0900

    自動更新機能追加

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -45,4 +45,48 @@ $(function(){
       alert('error');
     })
   })
-})
+
+  function buildMESSAGE(new_message){
+    var insertImage = '';
+    if (new_message.image_url) {
+      insertImage = `<img src="${new_message.image_url}">`;
+    }
+    var html = `<div class="message" data-message_id= ${new_message.id} >
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${new_message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${new_message.created_at}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${new_message.content}
+                    </p>
+                      ${insertImage}
+                  </div>
+                </div>`
+    $('.messages').append(html)
+  }
+  setInterval(update, 5000);
+  function update(){
+    if($('.message')[0]){
+      var message_id = $('.message:last').data('message_id');
+    } else {
+      var message_id = 0
+    }
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: { id: message_id },
+      dataType: 'json'
+    })
+    .done(function(new_message){
+      new_message.messages.forEach(function(autoMessage){
+        console.log(autoMessage)
+        buildMESSAGE(autoMessage);
+      });
+    });
+  }
+});
