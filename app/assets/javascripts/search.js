388969757bd6080acfef6ef00922d7ee99a3d8c3
ide-ryunosuke <r.i1996423@gmail.com>
2019-03-01 13:09:36 +0900

    インクリメンタルサーチ修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -15,7 +15,7 @@ $(function() {
 
   function appendMemberHTML(name,user_id) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
                   <p class='chat-group-user__name'>${name}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
@@ -47,6 +47,7 @@ $(function() {
     var name = $(this).attr("data-user-name");
     $(this).parent().remove();
     appendMemberHTML(name,user_id)
+    console.log(user_id)
   });
   $("#chat-group-users").on("click",".chat-group-user",function(){
     $(this).remove();
