fceff07957d91dedbb7ebd02bc38db2c87d4b5f0
ide-ryunosuke <r.i1996423@gmail.com>
2019-03-01 19:04:39 +0900

    Id設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,8 +19,8 @@ class GroupsController < ApplicationController
   end
 
   def update
-   if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    if @group.update(group_params)
+       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
